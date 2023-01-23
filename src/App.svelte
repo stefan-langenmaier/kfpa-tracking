1201c826cdc7b3708c4fbfacbe823c1d78cb325e
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2023-01-23 16:06:57 +0100

    minimal example with an object tracking hiro marker

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,33 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from 'svelte';
+  import '@ar-js-org/ar.js/aframe/build/aframe-ar'
+
+  // onMount(() => {
+	// 	const sceneEl = document.querySelector('a-scene');
+
+  //   console.log("adding listeners...");
+  //   console.log(sceneEl);
+  //   sceneEl.addEventListener('markerFound', () => {
+  //       console.log("myMarkerFound");
+  //   });
+
+  //   sceneEl.addEventListener('markerLost', () => {
+  //       console.log("myMarkerLost");
+  //   });
+
+  //   sceneEl.addEventListener('loaded', () => {
+  //       console.log("myLoaded");
+  //   });
+  //   console.log("added listeners");
+	// });
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<a-scene embedded arjs stats inspector vr-mode-ui="enabled: false;">
+  <a-marker preset="hiro">
+    <a-cone position='0 0.5 0' rotation="0 0 0" radius-bottom="0.5" material='opacity: 0.75; color: red;'></a-cone>
+  </a-marker>
+  <a-camera position="0 0 0"></a-camera>
+</a-scene>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
