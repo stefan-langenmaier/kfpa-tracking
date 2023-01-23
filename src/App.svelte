b0584c9de1dfdc223235c519eccd680271305037
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2023-01-23 16:06:57 +0100

    fix working diff between mobile and desktop
    probably the camera is generated at a different time on mobile and than a default camera is used
    ignoring the configuration
    using active: true should restore that

--- src/App.svelte
+++ src/App.svelte
@@ -2,31 +2,31 @@
   import { onMount } from 'svelte';
   import '@ar-js-org/ar.js/aframe/build/aframe-ar'
 
-  // onMount(() => {
-	// 	const sceneEl = document.querySelector('a-scene');
+  onMount(() => {
+		const sceneEl = document.querySelector('a-scene');
 
-  //   console.log("adding listeners...");
-  //   console.log(sceneEl);
-  //   sceneEl.addEventListener('markerFound', () => {
-  //       console.log("myMarkerFound");
-  //   });
+    console.log("adding listeners...");
+    console.log(sceneEl);
+    sceneEl.addEventListener('markerFound', () => {
+        console.log("myMarkerFound");
+    });
 
-  //   sceneEl.addEventListener('markerLost', () => {
-  //       console.log("myMarkerLost");
-  //   });
+    sceneEl.addEventListener('markerLost', () => {
+        console.log("myMarkerLost");
+    });
 
-  //   sceneEl.addEventListener('loaded', () => {
-  //       console.log("myLoaded");
-  //   });
-  //   console.log("added listeners");
-	// });
+    sceneEl.addEventListener('loaded', () => {
+        console.log("myLoaded");
+    });
+    console.log("added listeners");
+	});
 </script>
 
-<a-scene embedded arjs stats inspector vr-mode-ui="enabled: false;">
+<a-scene embedded arjs stats vr-mode-ui="enabled: false;">
   <a-marker preset="hiro">
     <a-cone position='0 0.5 0' rotation="0 0 0" radius-bottom="0.5" material='opacity: 0.75; color: red;'></a-cone>
   </a-marker>
-  <a-camera position="0 0 0"></a-camera>
+  <a-camera look-controls="enabled: false; active: true;" position="0 0 0"></a-camera>
 </a-scene>
 
 <style>
